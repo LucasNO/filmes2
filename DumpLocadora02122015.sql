5b1689355cf3616ae6557a660621f4d15c834d82
José Nicodemos Maia Neto <jose@nicomaia.com.br>
2015-12-03 12:50:39 -0200

    - Atualizado DUMP

--- DumpLocadora02122015.sql
+++ DumpLocadora02122015.sql
@@ -1,10 +1,10 @@
 CREATE DATABASE  IF NOT EXISTS `locadora` /*!40100 DEFAULT CHARACTER SET utf8 */;
 USE `locadora`;
--- MySQL dump 10.13  Distrib 5.6.17, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 5.6.23, for Win64 (x86_64)
 --
--- Host: localhost    Database: locadora
+-- Host: 127.0.0.1    Database: fatec_filmes
 -- ------------------------------------------------------
--- Server version	5.6.21-log
+-- Server version	5.6.24
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -115,7 +115,7 @@ CREATE TABLE `per_permissao` (
 
 LOCK TABLES `per_permissao` WRITE;
 /*!40000 ALTER TABLE `per_permissao` DISABLE KEYS */;
-INSERT INTO `per_permissao` VALUES (1,'ROLE_ADMIN'),(2,'ROLE_USUARIO');
+INSERT INTO `per_permissao` VALUES (1,'ADMIN'),(2,'USUARIO'),(3,'FUNCIONARIO');
 /*!40000 ALTER TABLE `per_permissao` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -135,7 +135,7 @@ CREATE TABLE `pus_permissao_usuario` (
   KEY `fk_PUS_PERMISSAO_USUARIO_2` (`PER_ID`),
   CONSTRAINT `fk_PUS_PERMISSAO_USUARIO_1` FOREIGN KEY (`USR_ID`) REFERENCES `usr_usuario` (`USR_ID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `fk_PUS_PERMISSAO_USUARIO_2` FOREIGN KEY (`PER_ID`) REFERENCES `per_permissao` (`PER_ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -144,7 +144,7 @@ CREATE TABLE `pus_permissao_usuario` (
 
 LOCK TABLES `pus_permissao_usuario` WRITE;
 /*!40000 ALTER TABLE `pus_permissao_usuario` DISABLE KEYS */;
-INSERT INTO `pus_permissao_usuario` VALUES (4,3,2),(5,1,1),(8,4,1),(11,2,1),(14,6,2);
+INSERT INTO `pus_permissao_usuario` VALUES (4,3,2),(5,1,1),(11,2,1),(14,6,1),(15,6,2),(16,6,3),(17,12,2),(18,12,3),(19,4,1),(20,4,2),(21,4,3);
 /*!40000 ALTER TABLE `pus_permissao_usuario` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -160,7 +160,7 @@ CREATE TABLE `usr_usuario` (
   `USR_SENHA` varchar(45) NOT NULL,
   `USR_NOME` varchar(45) NOT NULL,
   PRIMARY KEY (`USR_ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -169,9 +169,13 @@ CREATE TABLE `usr_usuario` (
 
 LOCK TABLES `usr_usuario` WRITE;
 /*!40000 ALTER TABLE `usr_usuario` DISABLE KEYS */;
-INSERT INTO `usr_usuario` VALUES (1,'123','thiago'),(2,'123','lucas'),(3,'123','tarcisio'),(4,'123','ronan'),(6,'123','teste2');
+INSERT INTO `usr_usuario` VALUES (1,'123','thiago'),(2,'123','lucas'),(3,'123','tarcisio'),(4,'123','ronan'),(6,'123','nicodemos'),(12,'123','teste');
 /*!40000 ALTER TABLE `usr_usuario` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'fatec_filmes'
+--
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -182,4 +186,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2015-12-02 12:55:52
+-- Dump completed on 2015-12-03 12:49:12
